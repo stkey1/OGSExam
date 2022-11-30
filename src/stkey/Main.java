744a9d7a119631e3269c7d008ebbc3a403590f13
stkey1 <st_key@hotmail.com>
2022-11-30 22:21:55 +0300

    Version 0.0.1 OGS EXAM

--- src/stkey/Main.java
+++ src/stkey/Main.java
@@ -6,19 +6,21 @@ package stkey;
  * @since 30.11.2022
  */
 public class Main {
+    private static java.lang.Object Object;
+
     /**
      * @param args
      */
     public static void main(String[] args) {
+        Object object;
         //Created BoxOfficeManager' object
         BoxOfficeManager boxOfficeManager = new BoxOfficeManager();
         boxOfficeManager.BoxOfficeManager();
         //For test, empty object is created from EmptyClass
-//        EmptyObject emptyObject = new EmptyObject();
-//        FirstClass car = new FirstClass();
-//        //Created BoxOfficeManager2's object
-//        BoxOfficeManager2 boxOfficeManager2 = new BoxOfficeManager2();
-//        boxOfficeManager2.BoxOfficeManager2(emptyObject);
-//        boxOfficeManager2.BoxOfficeManager2(car);
+        FirstClass car = new FirstClass();
+        //Created BoxOfficeManager2's object
+        BoxOfficeManager2 boxOfficeManager2 = new BoxOfficeManager2();
+        boxOfficeManager2.BoxOfficeManager2(Object);
+        boxOfficeManager2.BoxOfficeManager2(car);
     }
 }
