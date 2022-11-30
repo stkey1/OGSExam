724ef700b5fe126e50dd7994ac39b055ff105540
stkey1 <st_key@hotmail.com>
2022-11-30 22:15:33 +0300

    Version 0.0.1 OGS EXAM

--- src/stkey/Result.java
+++ src/stkey/Result.java
@@ -1,5 +1,10 @@
 package stkey;
 
+/**
+ * This method is enum for shorten sout
+ * @author stkey
+ * @since 30.11.2022
+ */
 public enum Result {
     TOTALVEHICLE("Total numbers of vehicles: "),
     REVENUE("Revenues for vehicles: "),
@@ -9,9 +14,7 @@ public enum Result {
     BALANCEPENALTY("Penalty Fee because of Having Not inadequate Balance: "),
     ACAR("Payment for a car: "),
     AMINIBUS("Payment for a minibus: "),
-    ABUS("Payment for a bus: ")
-
-    ;
+    ABUS("Payment for a bus: ");
     String s;
     Result(String s) {
         this.s = s;
